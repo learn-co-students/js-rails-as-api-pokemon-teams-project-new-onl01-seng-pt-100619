7e128be2dfa8b4686b0321420a2c5a77d5b47d76
ZumbaIdol <david.a.foote@gmail.com>
2020-05-12 18:27:56 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,37 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+document.addEventListener(DOMContentLoaded, function() {
+    fetchTrainers()
+})
+
+function fetchTrainers() {
+    console.log('loaded')
+    fetch(TRAINERS_URL)
+    .then(function(resp) {
+        return resp.json()
+    }).then(function(trainers) {
+        console.log(trainers)
+        for (const trainer of trainers) {
+            makeTrainerCard(trainer)
+        } 
+    })
+
+    function makeTrainerCard() {
+        const card = document.createElement('div')
+        card.classList += 'card'
+        card.dataset['id'] = trainer.id
+
+        const trainerName = document.createElement('a')
+        trainerName.innerText = trainer.name
+        card.appendChild(trainerName)
+
+        const addPokeButton = document.createElement('button')
+        addPokeButton.setAttribute('data-trainer-id', trainer.id)
+
+
+
+    }
+}
