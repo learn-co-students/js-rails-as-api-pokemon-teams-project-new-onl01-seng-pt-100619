69b9024a97fdff682674941db631faaa99dc24a6
allisoncortez <allizetorc@gmail.com>
2020-05-13 18:55:06 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,74 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector("main")
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchTrainers()
+})
+
+function fetchTrainers(){
+    fetch(TRAINERS_URL)
+    // .then(function(resp){
+    //     return resp.json()
+    // })
+    // .then(function(trainers){
+    //     for (const trainer of trainers){
+    //         makeTrainerCard(trainer)
+    //     }
+    // })
+
+    // v2
+    .then(resp => resp.json())
+    .then(json => {
+        json.forEach(trainer => makeTrainerCard(trainer))
+    })
+}
+
+function makeTrainerCard(trainer){
+    // CARD
+    const card = document.createElement('div')
+    card.classList = "card"
+    card.setAttribute("data-id", trainer.id)
+
+    //PTAG
+    const trainerName = document.createElement('p')
+    trainerName.innerText = trainer.name 
+    card.appendChild(trainerName)
+
+    //ADD POKE BUTTON
+    const addPokeButton = document.createElement('button')
+    addPokeButton.setAttribute("data-trainer-id", trainer.id) 
+    addPokeButton.innerText = "Add Pokemon"
+    card.appendChild(addPokeButton)
+    // attach event listener to button(click)
+
+    //add Pokemon Team
+    const pokemonList = document.createElement('ul')
+    // for (const pokemon of trainer.pokemons){
+    //     renderPokemon()
+    // }
+
+    
+    
+    card.appendChild(pokemonList)
+    main.appendChild(card) 
+    trainer.pokemons.forEach(pokemon => {
+        renderPokemon(pokemon)
+    })
+}
+
+function renderPokemon(pokemon){
+    const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+    const li = document.createElement("li")
+
+    li.innerText = `${pokemon.nickname} (${pokemon.species})`
+    
+    const button = document.createElement("button")
+    button.classList = "release"
+    button.setAttribute("data-pokemon-id", pokemon.id)
+    button.innerText = "Release"
+    li.appendChild(button)
+
+    ul.appendChild(li)
+}
