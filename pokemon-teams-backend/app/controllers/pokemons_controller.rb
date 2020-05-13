2abc3f0567558990a2451a969707c4f020938e1b
JTSwisher <jefftswisher@gmail.com>
2020-05-12 17:57:26 -0700

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,16 @@
+class PokemonsController < ApplicationController
+
+
+    def create
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+    end 
+
+    def destroy
+        pokemon = Pokemon.find_by(id: params[:id])
+        pokemon.delete
+    end 
+
+
+end
